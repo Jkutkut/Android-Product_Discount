ebc94eaa4b3309f2365f56c5ee79ba83280c5ce1
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-25 14:10:09 +0100

    Basic communication between Activities implemented

--- app/src/main/java/com/jkutkut/productdiscount/MainActivity.java
+++ app/src/main/java/com/jkutkut/productdiscount/MainActivity.java
@@ -1,14 +1,66 @@
 package com.jkutkut.productdiscount;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ******** UI Components ********
+    private Button btnSelectProduct;
+    private TextView txtvProduct;
+    private EditText etxtDiscount;
+    private TextView txtvDiscount;
+    private Button btnCalculate;
+
+    private ActivityResultLauncher<Intent> selectProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ******** UI Components ********
+        btnSelectProduct = findViewById(R.id.btnSelectProduct);
+        txtvProduct = findViewById(R.id.txtvProduct);
+        etxtDiscount = findViewById(R.id.etxtDiscount);
+        txtvDiscount = findViewById(R.id.txtvDiscount);
+        btnCalculate = findViewById(R.id.btnCalculate);
+
+        selectProduct = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        toastUser("Product selected");
+                        // Get data from intent
+                        String test = result.getData().getStringExtra("test");
+                        toastUser(test);
+                    }
+                    else {
+                        // TODO
+                    }
+                }
+        );
+
+        btnSelectProduct.setOnClickListener(v -> {
+            Intent i = new Intent(this, ProductSelection.class);
+            selectProduct.launch(i);
+        });
+
+        btnCalculate.setOnClickListener(v -> {
+            // TODO
+        });
+    }
+
+    private void toastUser(String str) {
+        if (str != null)
+            Toast.makeText(this, str, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/jkutkut/productdiscount/ProductSelection.java
+++ app/src/main/java/com/jkutkut/productdiscount/ProductSelection.java
@@ -2,13 +2,51 @@ package com.jkutkut.productdiscount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProductSelection extends AppCompatActivity {
+
+    // ******** UI Components ********
+    private EditText etxtCode;
+    private EditText etxtName;
+    private EditText etxtDesc;
+    private EditText etxtPrice;
+    private Button btnSearch;
+    private Button btnOk;
+    private Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_selection);
+
+        // ******** UI Components ********
+        etxtCode = findViewById(R.id.etxtCode);
+        etxtName = findViewById(R.id.etxtName);
+        etxtDesc = findViewById(R.id.etxtDesc);
+        etxtPrice = findViewById(R.id.etxtPrice);
+        btnSearch = findViewById(R.id.btnSearch);
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
+
+
+        btnOk.setOnClickListener(v -> handleOk());
+
+        btnCancel.setOnClickListener(v -> {
+            setResult(RESULT_CANCELED);
+            finish();
+        });
+    }
+
+    private void handleOk() {
+        // TODO
+
+        Intent data = new Intent();
+        data.putExtra("test", "testValue");
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
